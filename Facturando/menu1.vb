49861b76be888bec7c5a27dbc486e9426c7ed260
CharlsHL <71104832+CharlsHL@users.noreply.github.com>
2021-01-13 18:40:52 -0300

    logo nuevo imagen  nuevas funcionalidas

--- Facturando/menu1.vb
+++ Facturando/menu1.vb
@@ -60,4 +60,9 @@
             End
         End If
     End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Hide()
+        ctrlcliente.Show()
+
+    End Sub
 End Class
